8fa8b03fd314f1bcbbed9ac4dfee627c2c31ab1a
Mateus Maffi <mateusmaffi@fxsistemas.com.br>
2025-07-18 09:31:11 -0300

    Adjust sw-register

--- javascript/sw-register.js
+++ javascript/sw-register.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/javascript/service-worker.js')
+    .register('/MusicGallery/javascript/service-worker.js')
     .then(() => console.log('✅ Service Worker registrado!'))
     .catch(err => console.error('❌ Erro ao registrar SW:', err));
 }
