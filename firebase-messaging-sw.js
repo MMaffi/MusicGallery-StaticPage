291b4b2c125d2f34e64633cc82f52c22b2e20984
Mateus Maffi <mateusmaffi@fxsistemas.com.br>
2025-07-29 15:19:01 -0300

    notification update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -16,8 +16,32 @@ messaging.onBackgroundMessage((payload) => {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: './assets/favicon/web-app-manifest-192x192.png'
+        icon: './assets/favicon/web-app-manifest-192x192.png',
+        data: {
+            url: new URL('/MusicGallery', self.location.origin).href
+        }
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+// abrir a URL correta ao clicar na notificação
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+
+            for (const client of clientList) {
+                if (client.url === event.notification.data.url && 'focus' in client) {
+                return client.focus();
+                }
+            }
+
+            if (clients.openWindow) {
+                return clients.openWindow(event.notification.data.url);
+            }
+            
+        })
+    );
 });
